22b453cfe836e320da44cd04bc70b74ae278f234
HY <2356048622@qq.com>
2020-07-04 19:07:37 +0800

    feat(code):极客时间课程示例代码

---
+++ Demos/MusicPlayerSkinTest/app/src/main/java/com/yu/hu/musicplayerskintest/skin/PlayerSkin.kt
@@ -0,0 +1,8 @@
+package com.yu.hu.musicplayerskintest.skin
+
+/**
+ *@author Hy
+ *created on 2020/07/04 18:00
+ **/
+class PlaayerSkin {
+}
--- Demos/README.md
+++ Demos/README.md
@@ -1,4 +1,5 @@
 Kotlin Demos
 ---------------------
 
-* 
+* [极客时间课程代码](/KotlinPrimer-master)
+* [音乐播放器(皮肤切换)Demo](/MusicPlayerSkinTest)
